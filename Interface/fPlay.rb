0d92377f4fca5be1536373b4f08196f96b51a515
Yan72 <yann.paulmery.etu@univ-lemans.fr>
2019-03-29 16:32:01 +0100

    boutins en plus pour les aides (boutons non fonctionnels)

--- Interface/fPlay.rb
+++ Interface/fPlay.rb
@@ -2,6 +2,7 @@ require 'gtk3'
 require './Page.rb'
 
 require '../Classes/Grille.rb'
+require '../Classes/Aide.rb'
 require './boutonGrille.rb'
 
 class FPlay < Page
@@ -76,10 +77,61 @@ class FPlay < Page
 		@Label = Gtk::Label.new('Timer / afficher les aides')
 		@gHelp.add(@Label)
 
-		@bAide = Gtk::Button.new()
-		@help=(Gtk::Image.new(:file =>"./image/Aide.png"))
-		@bAide.set_image(@help)
-		@gHelp.add(@bAide)
+		@boxAide = Gtk::ButtonBox.new(:vertical)
+
+		@box1 = Gtk::ButtonBox.new(:horizontal)
+		@b1 = Gtk::Button.new(:label => '1', :use_underline => nil, :stock_id => nil)
+		@b2 = Gtk::Button.new(:label => '2', :use_underline => nil, :stock_id => nil)
+
+		@box1.add(@b1)
+		@box1.add(@b2)
+
+		@box2 = Gtk::ButtonBox.new(:horizontal)
+		@b3 = Gtk::Button.new(:label => '3', :use_underline => nil, :stock_id => nil)
+		@b4 = Gtk::Button.new(:label => '4', :use_underline => nil, :stock_id => nil)
+
+		@box2.add(@b3)
+		@box2.add(@b4)
+
+		@box3 = Gtk::ButtonBox.new(:horizontal)
+		@b5 = Gtk::Button.new(:label => '5', :use_underline => nil, :stock_id => nil)
+		@b6 = Gtk::Button.new(:label => '6', :use_underline => nil, :stock_id => nil)
+
+		@box3.add(@b5)
+		@box3.add(@b6)
+
+		@boxAide.add(@box1)
+		@boxAide.add(@box2)
+		@boxAide.add(@box3)
+
+		@b1.signal_connect('clicked') {
+			
+         	}
+
+		@b2.signal_connect('clicked') {
+
+         	}
+
+		@b3.signal_connect('clicked') {
+
+         	}
+
+		@b4.signal_connect('clicked') {
+
+         	}
+
+		@b5.signal_connect('clicked') {
+
+         	}
+
+		@b6.signal_connect('clicked') {
+
+         	}
+
+		#@bAide = Gtk::Button.new()
+#		@help=(Gtk::Image.new(:file =>"./image/Aide.png"))
+#		@bAide.set_image(@help)
+		@gHelp.add(@boxAide)
 
 		@bPause = Gtk::Button.new()
 		@pause=(Gtk::Image.new(:file =>"./image/pause.png"))

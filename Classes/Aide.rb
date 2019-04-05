4455a063c3b48ccd5835ff0c13bef65edae008f2
fata72 <chausette74@hotmail.fr>
2019-04-05 10:54:07 +0200

    Implémentation de la RDOC dans toutes les classes du dossier Classes

--- Classes/Aide.rb
+++ Classes/Aide.rb
@@ -1,11 +1,13 @@
 require_relative 'Grille.rb'
 
-#La classe Aide contient des méthodes qui évaluent l'état de la grille du joueur
+#====== La classe Aide contient des méthodes qui évaluent l'état de la grille du joueur
 class Aide
 
+	
 	#Parcourt la grille du joueur et la grille finale pour repérer les erreurs de placement
-	#Renvoie la première case rencontrée si elle n'est pas vide et ne correspond pas à la case finale
-	#Renvoie nil si toutes les cases sont correctes
+	# @param grille		//Prend la grille de jeu en paramètre
+	# @return case		//Renvoie la première case rencontrée si elle n'est pas vide et ne correspond pas à la case finale attendu
+	# @return nil		//Renvoie nil si toutes les cases sont correctes
 	def Aide.erreur(grille)
 		for i in 0..(grille.taille-1)
 			for j in 0..(grille.taille-1)
@@ -19,9 +21,12 @@ class Aide
 		return nil
 	end
 
+	
+
 	#Parcourt chaque ligne de la grille du joueur et repère les lignes qui doivent être remplies par des herbes
-	#Renvoie la première ligne rencontrée qui doit être complétée par des herbes
-	#Renvoie nil si aucune des lignes de la grille ne peut être complétée
+	# @param grille		//Prend la grille de jeu en paramètre
+	# @return ligne		//Renvoie la première ligne rencontrée qui doit être complétée par des herbes
+	# @return nil		//Renvoie nil si aucune des lignes de la grille ne peut être complétée
 	def Aide.ligneCompleterHerbes(grille)
 
 		i = 0
@@ -48,9 +53,12 @@ class Aide
 		return nil
 	end
 
+
+
 	#Parcourt chaque colonne de la grille du joueur et repère les colonnes qui doivent être remplies par des herbes
-	#Renvoie la première colonne rencontrée qui doit être complétée par des herbes
-	#Renvoie nil si aucune des colonnes de la grille ne peut être complétée
+	# @param grille		//Prend la grille de jeu en paramètre
+	# @return colonne		//Renvoie la première colonne rencontrée qui doit être complétée par des herbes des herbes
+	# @return nil		//Renvoie nil si aucune des colonnes de la grille ne peut être complétée
 	def Aide.colonneCompleterHerbes(grille)
 
 		i = 0
@@ -78,9 +86,11 @@ class Aide
 	end
 
 	
+
 	#Vérifie si les cases autour des tentes sont complétées
-	#Retourne la première tente rencontrée avec ses alentours non complétés
-	#Retourne nil si tous les alentours de chaque tente ont étés complétés 
+	# @param grille		//Prend la grille de jeu en paramètre
+	# @return tente		//Retourne la première tente rencontrée avec ses alentours non complétés
+	# @return nil		//Retourne nil si tous les alentours de chaque tente ont étés complétés
 	def Aide.tenteContourCompleter(grille)
 		caseTrouvee = nil
 		grille.parcourirH() { |uneCase|
@@ -100,9 +110,11 @@ class Aide
 	end
 
 
+
 	#Vérifie pour chaque arbre si il ne reste qu'une possibilité pour placer une tente
-	#Retourne le premier arbre rencontré qui ne possède pas de tente et un seul choix restant pour la placer
-	#Retourne nil si il n'y a aucun arbre avec un seul choix possible
+	# @param grille		//Prend la grille de jeu en paramètre
+	# @return arbre		//Retourne le premier arbre rencontré qui ne possède pas de tente et un seul choix restant pour la place
+	# @return nil		//Retourne nil si il n'y a aucun arbre avec un seul choix possible
 	def Aide.arbreTentePlacer(grille)
 		arbreTrouve = nil
 		grille.parcourirH() { |uneCase|
@@ -129,9 +141,11 @@ class Aide
 	end
 
 
+
 	#Vérifie si un des angles de chaque arbre est obligatoirement de l'herbe
-	#Retourne le premier arbre rencontré avec un angle contenant forcément de l'herbe
-	#Retourne nil sinon
+	# @param grille		//Prend la grille de jeu en paramètre
+	# @return arbre		//Retourne le premier arbre rencontré avec un angle contenant forcément de l'herbe
+	# @return nil		//Retourne nil sinon
 	def Aide.arbreAngleHerbe(grille)
 		arbreTrouve = nil
 		grille.parcourirH() { |uneCase|
--- Classes/Score.rb
+++ Classes/Score.rb
@@ -1,20 +1,32 @@
+#====== La Classe Score définie les critère de calcul du score en fonction de la grille etc
+
 class Score
 
-	#@difficulte	La difficulté de la grille
-	#@scoreInitial	Le score initial si la grille est complétée
-	#@nbFeuilles	Le nombre de feuilles
+	#=Variables d'instance
+	# @difficulte		: La difficulté de la grille
+	# @scoreInitial		: Le score initial si la grille est complétée
+	# @nbFeuilles		: Le nombre de feuilles (qui déterminera le nombre d'aide disponsible)
 
 
-	attr_reader :difficulte, :scoreInitial, :nbFeuilles
+	attr_reader :scoreInitial
+	attr_reader :nbFeuilles
+	attr_reader :difficulte
 
-	#Créé un score
+
+	#Création d'un score
+	# @param diff		//difficulté de la grille
+	# @param nbF		//nombre de feuille pour la grille
+	# @return void		//ne renvoie rien
 	def Score.creer (diff, nbF)
 		new(diff,nbF)
 	end
 
 	private_class_method :new
 
-
+	#Initialisation
+	# @param diff		//difficulté de la grille
+	# @param nbF		//nombre de feuille pour la grille
+	# @return void		//ne renvoie rien
 	def initialize (diff, nbF)
 		@difficulte = diff
 		case diff
@@ -28,14 +40,21 @@ class Score
 		@nbFeuilles = nbF
 	end
 
+	
+	
 	#Calcule le score en fonction de la difficulté, du temps restant et du nombre de feuilles restantes
-	#Param tempsRestant Le temps restant
+	# @param tempsRestant		//Le temps restant à la fin de la partie
+	# @return score			//Le score calculé
 	def calculerScore (tempsRestant)
 		return @scoreInitial + tempsRestant + @nbFeuilles*10
 	end
 
-	#Tri les scores par ordre décroissant
+	
 	#Renvoie le tableau des scores trié
+
+	#Tri les scores par ordre décroissant
+	# @param tabScores		//tableau comportant les scores
+	# @return tab		//un tableau de score trié
 	def Score.triScores (tabScores)
 		return tabScores.sort.reverse
 	end

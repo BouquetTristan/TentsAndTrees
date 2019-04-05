d2d482c7804d71d5f72906e8a892981249337354
fata72 <chausette74@hotmail.fr>
2019-04-05 09:52:53 +0200

    création de classe Test pour les deux chrono

---
+++ Classes/ChronoTest.rb
@@ -0,0 +1,20 @@
+require_relative 'Chrono.rb'
+
+#c=Chrono.new()
+
+#thr=Thread.new{
+#			c.start
+ #}
+
+
+#sleep(3)
+#c.pause()
+#sleep(3)
+#c.pause()
+#print "Saisi"
+#s = gets
+#sleep(3)
+#print s
+#sleep(3)
+#c.raz()
+#sleep(5)

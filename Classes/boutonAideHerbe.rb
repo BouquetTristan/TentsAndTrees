361095a8ef7ab97c010000d4ca664dc68875bbbc
Yan72 <yann.paulmery.etu@univ-lemans.fr>
2019-04-22 16:23:07 +0200

    Suite mise au point

--- Classes/boutonAideHerbe.rb
+++ Classes/boutonAideHerbe.rb
@@ -43,31 +43,31 @@ class BoutonAideHerbe < BoutonAide
 			aide4 = Aide.arbreAngleHerbe(uneGrille)
 			aide5 = Aide.caseEstDeLHerbe(uneGrille)
 			if(aide1 != nil)
-				unLabel.set_markup("<span foreground=\"#FFFFFF\" font-desc=\"Courier New bold 11\">La colonne #{aide1} peut être \ncomplétée par des herbes</span>")
+				unLabel.set_markup("<span foreground=\"#E30E0B\" font-desc=\"Courier New bold 11\">La colonne #{aide1} peut être \ncomplétée par des herbes</span>")
 				for i in (0..uneGrille.taille-1)
 					interfaceGrille[aide1][i].indiquerAide(uneGrille.grilleJ[aide1][i].etat)
 				end
 				return aide1
 			elsif(aide2 != nil)
-				unLabel.set_markup("<span foreground=\"#FFFFFF\" font-desc=\"Courier New bold 11\">La ligne #{aide2} peut être \ncomplétée par des herbes</span>")
+				unLabel.set_markup("<span foreground=\"#E30E0B\" font-desc=\"Courier New bold 11\">La ligne #{aide2} peut être \ncomplétée par des herbes</span>")
 				for i in (0..uneGrille.taille-1)
 					interfaceGrille[i][aide2].indiquerAide(uneGrille.grilleJ[i][aide2].etat)
 				end
 				return aide2
 			elsif(aide3 != nil)
-				unLabel.set_markup("<span foreground=\"#FFFFFF\" font-desc=\"Courier New bold 11\">Le contour de la tente en \n#{aide3.i} #{aide3.j} \ndoit être complété</span>")
+				unLabel.set_markup("<span foreground=\"#E30E0B\" font-desc=\"Courier New bold 11\">Le contour de la tente en \n#{aide3.i} #{aide3.j} \ndoit être complété</span>")
 				interfaceGrille[aide3.i][aide3.j].indiquerAide(uneGrille.grilleJ[aide3.i][aide3.j].etat)
 				return aide3
 			elsif(aide4 != nil)
-				unLabel.set_markup("<span foreground=\"#FFFFFF\" font-desc=\"Courier New bold 11\">L'arbre en #{aide4.i} #{aide4.j} \npossède un coin qui est obligatoirement de l'herbe</span>")
+				unLabel.set_markup("<span foreground=\"#E30E0B\" font-desc=\"Courier New bold 11\">L'arbre en #{aide4.i} #{aide4.j} \npossède un coin qui est obligatoirement de l'herbe</span>")
 				interfaceGrille[aide4.i][aide4.j].indiquerAide(uneGrille.grilleJ[aide4.i][aide4.j].etat)
 				return aide4
 			elsif(aide5 != nil)
-				unLabel.set_markup("<span foreground=\"#FFFFFF\" font-desc=\"Courier New bold 11\">La case  en #{aide5.i} #{aide5.j} \nne dépend d'aucun arbre et est donc \nforcément de l'herbe</span>")
+				unLabel.set_markup("<span foreground=\"#E30E0B\" font-desc=\"Courier New bold 11\">La case  en #{aide5.i} #{aide5.j} \nne dépend d'aucun arbre et est donc \nforcément de l'herbe</span>")
 				interfaceGrille[aide5.i][aide5.j].indiquerAide(uneGrille.grilleJ[aide5.i][aide5.j].etat)
 				return aide5
 			else
-				unLabel.set_markup("<span foreground=\"#FFFFFF\" font-desc=\"Courier New bold 11\">Aucune herbe \nne peut être placée</span>")
+				unLabel.set_markup("<span foreground=\"#E30E0B\" font-desc=\"Courier New bold 11\">Aucune herbe \nne peut être placée</span>")
 				return nil
 			end
 		end

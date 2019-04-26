77092152bac46a8347d94d8fc1c7f1472a622d92
Yan72 <Yann.Paulmery.etu@univ-lemans.fr>
2019-04-26 14:42:34 +0200

    Suppression du système d'installation de gems et de lancement du jeu

--- Rakefile
+++
@@ -1,6 +0,0 @@
-task default: %w[main]
-
-task :main do
-	ruby "BDD.rb"
-	ruby "Main.rb"
-end
---
+++ test.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo "Hello World"
+
+  sudo gem install gtk3
+  sudo gem install thread
+  sudo gem install sqlite3
+  ruby BDD.rb
+  ruby Main.rb
